5560ba906a1631e0792bf36a07015f5971eb99b1
gabrielpp0 <71361726+gabrielpp0@users.noreply.github.com>
2021-03-31 20:36:13 -0400

    first

---
+++ src/services/config.js
@@ -0,0 +1,21 @@
+/*
+  Ce fichier contient les variables de configuration
+  reliées à Firebase et ses produits.
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBSlXwTxpPtotdlCDMZUVaGCHzDPfBe7CM",
+  authDomain: "signet-4b02b.firebaseapp.com",
+  projectId: "signet-4b02b",
+  storageBucket: "signet-4b02b.appspot.com",
+  messagingSenderId: "832467340854",
+  appId: "1:832467340854:web:ac6e5dde706254b1289297",
+  measurementId: "G-CFBP1KN3V6"
+};
+export default firebaseConfig;
+
+// Nom de la collection principale
+export const utilRef = "utilisateurs-ex5";
+// Nom de la sous-collection
+export const dossRef = "dossiers";
